6827921b5df47f5200ed17ec4b76cc344cc65dc1
siued <matej.kucera.01@gmail.com>
2024-02-17 15:21:15 +0100

    db controller functions

--- assignment/db_connection.py
+++ assignment/db_connection.py
@@ -3,50 +3,72 @@ from dotenv import load_dotenv
 import os
 import json
 
-# Load environment variables from .env file
-load_dotenv()
 
-# Retrieve database connection details from environment variables
-host = "localhost"  # Docker is running on the same machine
-user = os.getenv("MYSQL_USER")
-password = os.getenv("MYSQL_PASSWORD")
-database = os.getenv("MYSQL_DATABASE")
+def get_connection():
+    load_dotenv()
 
-# Create a connection to the MySQL server
-connection = mysql.connector.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=database
-)
+    # Retrieve database connection details from environment variables
+    host = "localhost"  # Docker is running on the same machine
+    user = os.getenv("MYSQL_USER")
+    password = os.getenv("MYSQL_PASSWORD")
+    database = os.getenv("MYSQL_DATABASE")
 
-# Create a cursor object to interact with the database
-cursor = connection.cursor()
+    # Create a connection to the MySQL server
+    connection = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
 
-# Read data from the JSON file
-json_file_path = "assignment\\sample_set.json"
-with open(json_file_path, "r") as json_file:
-    data = json.load(json_file)
+    # Create a cursor object to interact with the database
+    cursor = connection.cursor()
+    return connection
 
-# Read the SQL file (file.sql) containing the CREATE TABLE statement
-sql_file_path = "assignment\\product.sql"  # Adjusted path
-with open(sql_file_path, "r") as sql_file:
-    create_table_query = sql_file.read()
 
-# Execute the CREATE TABLE statement
-cursor.execute(create_table_query, multi=True)
+def create_table():
+    connection = get_connection()
+    cursor = connection.cursor()
 
-# Insert data into the table
-columns_placeholder = ", ".join(['%s'] * len(data[0]))
-insert_data_query = f"INSERT INTO product ({', '.join(data[0].keys())}) VALUES ({columns_placeholder})"
+    # Read the SQL file (file.sql) containing the CREATE TABLE statement
+    sql_file_path = "assignment\\product.sql"  # Adjusted path
+    with open(sql_file_path, "r") as sql_file:
+        create_table_query = sql_file.read()
 
-for record in data:
-    data_to_insert = tuple(record.get(key, None) for key in data[0].keys())
-    cursor.execute(insert_data_query, data_to_insert)
+    # Execute the CREATE TABLE statement
+    cursor.execute(create_table_query, multi=True)
 
-# Commit the changes to the database
-connection.commit()
+    # Commit the changes to the database
+    connection.commit()
 
-# Close the cursor and connection
-cursor.close()
-connection.close()
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+def add_items(data):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    # Insert data into the table
+    columns_placeholder = ", ".join(['%s'] * len(data[0]))
+    insert_data_query = f"INSERT INTO product ({', '.join(data[0].keys())}) VALUES ({columns_placeholder})"
+
+    for record in data:
+        data_to_insert = tuple(record.get(key, None) for key in data[0].keys())
+        cursor.execute(insert_data_query, data_to_insert)
+
+    # Commit the changes to the database
+    connection.commit()
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+create_table()
+
+
+with open("assignment\\sample_set.json", "r") as file:
+    items = json.load(file)
+    add_items(items)
