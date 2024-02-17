9e8db8d5f4fe5dec75a547c5dd49abfdd4558b38
akselj <aksel.reedi@gmail.com>
2024-02-17 14:17:11 +0100

    update stock manager

--- assignment/stock-manager.py
+++ assignment/stock-manager.py
@@ -1,40 +1,29 @@
-import database_controller as database
+import database_controller as database  # Database Controller class that implements getters and setters
 import location_manager
-import numpy as np
 
 # import order retrieval
 # import new item retreival
 
 
-class Stock_Manager:
-    def __init__(self) -> None:
-        self.incoming_order = None  # A cron job?
-        self.new_item = None
-        self.database = None  # Database Controller class that implements getters and setters
-        self.warehouses = tuple(
-            "warehouse1", "warehouse2", "warehouse3", "warehouse4"
-        )
-        self.location_manager = (
-            None  # Location manager that generates the location for the item
-        )
+def add_new_item_to_database(item, warehouse):
+    db_location, _ = location_manager.create_new_item_location(item, warehouse)
 
-        self.items_in_warehouse = None  # database.get_all_items()
-        self.n_items_in_warehouse = len(self.items_in_warehouse)
-        self.location_of_item = str(None)
+    database.add_item(item)
+    database.add_location_to_item(item, db_location)
 
-    def add_new_item_to_database(self, item):
-        location = location_manager.create_new_item_location(item, warehouse)
-        database.add_location(item, location)
 
-    def add_order_to_database(self, order):
-        database.add_order(order)
+def add_order_to_database(order):
+    database.add_order(order)
 
-        pass
 
-    def get_item_from_database(self):
-        pass
+def get_item_from_database(item):
+    return database.get_item(item)
 
-    def get_item_location_from_database(self, item):
-        item_location = database.get_location(item)
 
-        return item_location
+def get_item_location_from_database(item):
+    return database.get_location(item)
+
+
+def remove_item_from_database(items_to_remove):
+    for item in items_to_remove:
+        database.remove_item(item)
