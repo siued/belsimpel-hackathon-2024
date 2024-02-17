cc07270e1b0d5ed5e7d010b963b77c809ee53c6e
siued <matej.kucera.01@gmail.com>
2024-02-17 11:35:36 +0100

    get_human_readable_location

--- assignment/warehouse.py
+++ assignment/warehouse.py
@@ -1,4 +1,23 @@
-def get_location_for_new_order(order):
-    id = order["id"]
+def get_location_for_new_order(item):
+    # create a new unique location for the order
+
+    id = item["id"]
     return str(id) + '-location';
 
+
+def get_human_readable_location(location, item):
+    # convert the location to a human readable format
+
+    id = location.replace('-location', '')
+
+    return 'Item ' + item['description'] + 'with id ' + id + ' is located at ' + location
+
+
+item = {
+        "id": "3526",
+        "description": "Google Pixel 8 128GB Black",
+        "IMEI": "331065863440734",
+        "EAN": "840244706692",
+        "price": "450.00"
+    }
+print(get_human_readable_location(get_location_for_new_order(item), item))
