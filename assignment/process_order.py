d3f3bbf10f7e3c1061c14b22deeeccb6c466b10d
Marcus Persson <marcusper2003@gmail.com>
2024-02-17 14:00:43 +0100

    process order basic

---
+++ assignment/process_order.py
@@ -0,0 +1,30 @@
+import stock_manager
+import verify_ean
+
+EAN = "EAN"
+
+def process_order(order):
+    eans = []
+    for i in order[1]:
+        eans.append[i[EAN]]
+    
+    print(eans)
+
+    items = 0
+    while items < len(order):
+        item = input("Scan the item (EAN tag): ")
+        if item == "exit":
+            print("Stopped processing order")
+            return
+        if verify_ean.ean_verified(item, eans):
+            print("Item scanned.")
+            items += 1
+        else:
+            print("Invalid EAN tag. Please scan again.")
+    print("All correct items scanned. Processing the order.")
+
+    stock_manager.remove_from_stock()
+    return
+        
+    
+    
